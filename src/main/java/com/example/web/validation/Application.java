bbebe14aee87856040508b7ed36ee18df1659186
wuzhihao-jk <wuzhihao-jk@360shuke.com>
2022-10-08 18:22:44 +0800

    validation

---
+++ src/main/java/com/example/web/validation/Application.java
@@ -0,0 +1,21 @@
+package com.example.web.validation;
+
+import com.example.web.errorhandling.ErrorHandlingVerticle;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+  private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new ValidationVerticle(), res -> {
+      if(res.succeeded()){
+        log.info("{} had deployed!", res.result());
+      } else {
+        log.error("deploy failed", res.cause());
+      }
+    });
+  }
+}
---
+++ src/main/java/com/example/web/validation/ValidationVerticle.java
@@ -0,0 +1,42 @@
+package com.example.web.validation;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.validation.RequestParameters;
+import io.vertx.ext.web.validation.ValidationHandler;
+import static io.vertx.ext.web.validation.builder.Parameters.param;
+import io.vertx.ext.web.validation.builder.ValidationHandlerBuilder;
+import io.vertx.json.schema.SchemaParser;
+import io.vertx.json.schema.SchemaRouter;
+import io.vertx.json.schema.SchemaRouterOptions;
+import static io.vertx.json.schema.common.dsl.Schemas.intSchema;
+import io.vertx.json.schema.draft7.Draft7SchemaParser;
+import static io.vertx.json.schema.draft7.dsl.Keywords.maximum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ValidationVerticle extends AbstractVerticle {
+  private static final Logger log = LoggerFactory.getLogger(ValidationVerticle.class);
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    Router router = Router.router(vertx);
+    router.route("/valid")
+      .handler(ValidationHandlerBuilder.create(Draft7SchemaParser.create(SchemaRouter.create(vertx, new SchemaRouterOptions()))).queryParameter(param("aParam", intSchema().with(maximum(100)))).build())
+      .handler(ctx -> {
+        RequestParameters params = ctx.get(ValidationHandler.REQUEST_CONTEXT_KEY);
+        ctx.response().end(params.queryParameter("aParam").getInteger().toString());
+      });
+
+    int port = 8080;
+    vertx.createHttpServer().requestHandler(router).listen(port, res -> {
+      if(res.succeeded()){
+        log.info("Http server started on port: {}", port);
+        startPromise.complete();
+      }else {
+        startPromise.fail(res.cause());
+      }
+    });
+  }
+}
